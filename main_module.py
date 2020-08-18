5510bd81c445893b78f8811624abc88084cf568c
Code/code113 <ashleyflinstone123@gmail.com>
2020-08-18 00:36:15 -0400

    Edit doc strings

--- main_module.py
+++ main_module.py
@@ -1,18 +1,12 @@
 from src.java_checks import *
-import subprocess
-import os
-import glob
-import shutil
-import time
-import json
 
 if __name__ == '__main__':
     print("Checking system os type...")
     OS = check_system_os()
     cur_java_version = 8
     print("Checking if java installed...")
-    # print(os.environ.items())  # 1. step 1 to check environment variables of system if key has value JAVA
-    # TO DO: Check for Ubuntu/linux
+
+    # TO DO: Install Java for Windows and Darwin
     if OS == 'Windows':  # check for windows
         flag, path = win_check_environ_java()
         if flag == 1:
@@ -28,9 +22,9 @@ if __name__ == '__main__':
         if path != 'Java not found':
             print('java exist -version ' + path)
         else:                                         #java not found install java
-            print("Java not installed, Installing Java...")
+            print("Java not installed")
 
-    elif OS == 'Linux':  # check Java installed on Mac OS
+    elif OS == 'Linux':  # check Java installed on Ubuntu system
         flag = check_java_linux()
         print("Flag- ", flag)
         if flag == 1:
