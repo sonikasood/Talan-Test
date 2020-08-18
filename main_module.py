adb72377d50a7fc5d6ec16f39ac251da0dfd1204
Code/code113 <ashleyflinstone123@gmail.com>
2020-08-17 22:49:06 -0400

    Structure of code changed(Refactor)

--- main_module.py
+++ main_module.py
@@ -1,6 +1,4 @@
-import sys
-sys.path.append('../src')
-from src import java_checks
+from src.java_checks import *
 import subprocess
 import os
 import glob
@@ -13,40 +11,40 @@ if __name__ == '__main__':
 
 
     print("Checking system os type...")
-    OS = java_checks.check_system_os()
+    OS = check_system_os()
     print("Checking if java installed...")
     # print(os.environ.items())  # 1. step 1 to check environment variables of system if key has value JAVA
     # TO DO: Check for Ubuntu/linux
     if OS == 'Windows':  # check for windows
-        flag, path = java_checks.win_check_environ_java()
+        flag, path = win_check_environ_java()
         if flag == 1:
             print("Java already configured in the system")
             print("The path is: " + path)
-            version = java_checks.java_version()  # getting java version for windows system
+            version = java_version()  # getting java version for windows system
             print("Java version: " + version)
         else:
             print("Java not found in the system")
 
     elif OS == 'Darwin':  # check Java installed on Mac OS
-        path = java_checks.darwin_check_environ_java()
+        path = darwin_check_environ_java()
         if path != 'Java not found':
             print('java exist -version ' + path)
         else:                                         #java not found install java
             print("Java not installed, Installing Java...")
 
     elif OS == 'Linux':  # check Java installed on Mac OS
-        flag = java_checks.check_java_linux()
+        flag = check_java_linux()
         print("Flag- ", flag)
         if flag == 1:
             print("Java not installed ")
-            java_checks.install_Java()
+            install_Java()
             print("java installed on This system")
         elif flag == 0:
             print("Java already configured in the system")
-            version = java_checks.java_version()  # getting java version for windows system
+            version = java_version()  # getting java version for windows system
             print("Java version: " + version)
 
-# java_checks.send_email()
+
 
 
 
